9894d6ab7a1a73aeec401657964628d6a163b206
jore lopez <jore.lopez@luckywoman.com.co>
2019-10-09 09:44:35 -0500

    lista productos base

---
+++ src/pages/product/basic/list/index.tsx
@@ -0,0 +1,370 @@
+import {
+  Avatar,
+  Button,
+  Card,
+  Col,
+  DatePicker,
+  Dropdown,
+  Form,
+  Icon,
+  Input,
+  List,
+  Menu,
+  Modal,
+  Progress,
+  Radio,
+  Row,
+  Select,
+  Result,
+} from 'antd';
+import React, { Component } from 'react';
+import Link from 'umi/link';
+import { Dispatch } from 'redux';
+import { FormComponentProps } from 'antd/es/form';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { connect } from 'dva';
+import { findDOMNode } from 'react-dom';
+import moment from 'moment';
+import { StateType } from './model';
+import { BasicListItemDataType } from './data.d';
+import styles from './style.less';
+
+const FormItem = Form.Item;
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+const SelectOption = Select.Option;
+const { Search, TextArea } = Input;
+
+interface ListProps extends FormComponentProps {
+  listBasicList: StateType;
+  dispatch: Dispatch<any>;
+  loading: boolean;
+}
+interface ListState {
+  visible: boolean;
+  done: boolean;
+  current?: Partial<BasicListItemDataType>;
+}
+@connect(
+  ({
+    listBasicList,
+    loading,
+  }: {
+    listBasicList: StateType;
+    loading: {
+      models: { [key: string]: boolean };
+    };
+  }) => ({
+    listBasicList,
+    loading: loading.models.listBasicList,
+  }),
+)
+class BasicList extends Component<
+  ListProps,
+  ListState
+> {
+  state: ListState = { visible: false, done: false, current: undefined };
+
+  formLayout = {
+    labelCol: { span: 7 },
+    wrapperCol: { span: 13 },
+  };
+
+  addBtn: HTMLButtonElement | undefined | null = undefined;
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'listBasicList/fetch',
+      payload: {
+        count: 5,
+      },
+    });
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+      current: undefined,
+    });
+  };
+
+  showEditModal = (item: BasicListItemDataType) => {
+    this.setState({
+      visible: true,
+      current: item,
+    });
+  };
+
+  handleDone = () => {
+    setTimeout(() => this.addBtn && this.addBtn.blur(), 0);
+    this.setState({
+      done: false,
+      visible: false,
+    });
+  };
+
+  handleCancel = () => {
+    setTimeout(() => this.addBtn && this.addBtn.blur(), 0);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const { dispatch, form } = this.props;
+    const { current } = this.state;
+    const id = current ? current.id : '';
+
+    setTimeout(() => this.addBtn && this.addBtn.blur(), 0);
+    form.validateFields((err: string | undefined, fieldsValue: BasicListItemDataType) => {
+      if (err) return;
+      this.setState({
+        done: true,
+      });
+      dispatch({
+        type: 'listBasicList/submit',
+        payload: { id, ...fieldsValue },
+      });
+    });
+  };
+
+  deleteItem = (id: string) => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'listBasicList/submit',
+      payload: { id },
+    });
+  };
+
+  render() {
+    const {
+      listBasicList: { list },
+      loading,
+    } = this.props;
+    const {
+      form: { getFieldDecorator },
+    } = this.props;
+
+    const { visible, done, current = {} } = this.state;
+
+    const editAndDelete = (key: string, currentItem: BasicListItemDataType) => {
+      if (key === 'edit') this.showEditModal(currentItem);
+      else if (key === 'delete') {
+        Modal.confirm({
+          title: '删除任务',
+          content: '确定删除该任务吗？',
+          okText: '确认',
+          cancelText: '取消',
+          onOk: () => this.deleteItem(currentItem.id),
+        });
+      }
+    };
+
+    const modalFooter = done
+      ? { footer: null, onCancel: this.handleDone }
+      : { okText: '保存', onOk: this.handleSubmit, onCancel: this.handleCancel };
+
+    const Info: React.FC<{
+      title: React.ReactNode;
+      value: React.ReactNode;
+      bordered?: boolean;
+    }> = ({ title, value, bordered }) => (
+      <div className={styles.headerInfo}>
+        <span>{title}</span>
+        <p>{value}</p>
+        {bordered && <em />}
+      </div>
+    );
+
+    const extraContent = (
+      <div className={styles.extraContent}>
+        <RadioGroup defaultValue="all">
+          <RadioButton value="all">全部</RadioButton>
+          <RadioButton value="progress">进行中</RadioButton>
+          <RadioButton value="waiting">等待中</RadioButton>
+        </RadioGroup>
+        <Search className={styles.extraContentSearch} placeholder="请输入" onSearch={() => ({})} />
+      </div>
+    );
+
+    const paginationProps = {
+      showSizeChanger: true,
+      showQuickJumper: true,
+      pageSize: 5,
+      total: 50,
+    };
+
+    const ListContent = ({
+      data: { owner, createdAt, percent, status },
+    }: {
+      data: BasicListItemDataType;
+    }) => (
+      <div className={styles.listContent}>
+        <div className={styles.listContentItem}>
+          <span>Owner</span>
+          <p>{owner}</p>
+        </div>
+        <div className={styles.listContentItem}>
+          <span>开始时间</span>
+          <p>{moment(createdAt).format('YYYY-MM-DD HH:mm')}</p>
+        </div>
+        <div className={styles.listContentItem}>
+          <Progress percent={percent} status={status} strokeWidth={6} style={{ width: 180 }} />
+        </div>
+      </div>
+    );
+
+    const MoreBtn: React.FC<{
+      item: BasicListItemDataType;
+    }> = ({ item }) => (
+      <Dropdown
+        overlay={
+          <Menu onClick={({ key }) => editAndDelete(key, item)}>
+            <Menu.Item key="edit">Desactivar</Menu.Item>
+            <Menu.Item key="delete">Borrar</Menu.Item>
+          </Menu>
+        }
+      >
+        <a>
+          Opt. <Icon type="down" />
+        </a>
+      </Dropdown>
+    );
+
+    const getModalContent = () => {
+      if (done) {
+        return (
+          <Result
+            status="success"
+            title="操作成功"
+            subTitle="一系列的信息描述，很短同样也可以带标点。"
+            extra={
+              <Button type="primary" onClick={this.handleDone}>
+                知道了
+              </Button>
+            }
+            className={styles.formResult}
+          />
+        );
+      }
+      return (
+        <Form onSubmit={this.handleSubmit}>
+          <FormItem label="任务名称" {...this.formLayout}>
+            {getFieldDecorator('title', {
+              rules: [{ required: true, message: '请输入任务名称' }],
+              initialValue: current.title,
+            })(<Input placeholder="请输入" />)}
+          </FormItem>
+          <FormItem label="开始时间" {...this.formLayout}>
+            {getFieldDecorator('createdAt', {
+              rules: [{ required: true, message: '请选择开始时间' }],
+              initialValue: current.createdAt ? moment(current.createdAt) : null,
+            })(
+              <DatePicker
+                showTime
+                placeholder="请选择"
+                format="YYYY-MM-DD HH:mm:ss"
+                style={{ width: '100%' }}
+              />,
+            )}
+          </FormItem>
+          <FormItem label="任务负责人" {...this.formLayout}>
+            {getFieldDecorator('owner', {
+              rules: [{ required: true, message: '请选择任务负责人' }],
+              initialValue: current.owner,
+            })(
+              <Select placeholder="请选择">
+                <SelectOption value="付晓晓">付晓晓</SelectOption>
+                <SelectOption value="周毛毛">周毛毛</SelectOption>
+              </Select>,
+            )}
+          </FormItem>
+          <FormItem {...this.formLayout} label="产品描述">
+            {getFieldDecorator('subDescription', {
+              rules: [{ message: '请输入至少五个字符的产品描述！', min: 5 }],
+              initialValue: current.subDescription,
+            })(<TextArea rows={4} placeholder="请输入至少五个字符" />)}
+          </FormItem>
+        </Form>
+      );
+    };
+    return (
+      <>
+        <PageHeaderWrapper>
+          <div className={styles.standardList}>
+            <Card bordered={false}>
+              <Row>
+                <Col sm={8} xs={24}>
+                  <Info title="Productos sin Stock" value="8" bordered />
+                </Col>
+                <Col sm={8} xs={24}>
+                  <Info title="Total productos" value="33" bordered />
+                </Col>
+                <Col sm={8} xs={24}>
+                 
+                  <Link to='/catalog/products/new' className='ant-btn ant-btn-link' >
+                    <Icon type='plus' /> Nuevo producto
+                  </Link>
+                </Col>
+              </Row>
+            </Card>
+
+            <Card
+              className={styles.listCard}
+              bordered={false}
+              title="Productos"
+              style={{ marginTop: 24 }}
+              bodyStyle={{ padding: '0 32px 40px 32px' }}
+              extra={extraContent}
+            >
+             
+              <List
+                size="large"
+                rowKey="id"
+                loading={loading}
+                pagination={paginationProps}
+                dataSource={list}
+                renderItem={item => (
+                  <List.Item
+                    actions={[
+                      <Link
+                        key="edit"
+                        to={`/catalog/products/${item.id}`}
+                      >
+                        Editar
+                      </Link>,
+                      <MoreBtn key="more" item={item} />,
+                    ]}
+                  >
+                    <List.Item.Meta
+                      avatar={<Avatar src={item.logo} shape="square" size="large" />}
+                      title={<a href={item.href}>{item.title}</a>}
+                      description={item.subDescription}
+                    />
+                    <ListContent data={item} />
+                  </List.Item>
+                )}
+              />
+            </Card>
+          </div>
+        </PageHeaderWrapper>
+
+        <Modal
+          title={done ? null : `任务${current ? '编辑' : '添加'}`}
+          className={styles.standardListForm}
+          width={640}
+          bodyStyle={done ? { padding: '72px 0' } : { padding: '28px 0 0' }}
+          destroyOnClose
+          visible={visible}
+          {...modalFooter}
+        >
+          {getModalContent()}
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default Form.create<ListProps>()(BasicList);
