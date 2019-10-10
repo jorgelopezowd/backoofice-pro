0ae780287f3a74fe0ee66c1c3c7d5b98b4e7432b
jore lopez <jore.lopez@luckywoman.com.co>
2019-10-10 10:30:18 -0500

    titulo multi idioma

--- src/pages/product/form/index.tsx
+++ src/pages/product/form/index.tsx
@@ -13,32 +13,34 @@ import {
   Tabs,
 } from 'antd';
 import React, { Component } from 'react';
-
+import { StickyContainer, Sticky } from 'react-sticky';
 import { Dispatch } from 'redux';
 import { FormComponentProps } from 'antd/es/form';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { connect } from 'dva';
-import TableForm from './components/TableForm';
+// import TableForm from './components/TableForm';
 import FooterToolbar from './components/FooterToolbar';
-import styles from './style.less';
 import ProductInfo from './components/info';
 import ProductStock from './components/stock';
 import ProductShipping from './components/shipping';
 import ProductPricing from './components/pricing';
 import ProductSeo from './components/seo';
+import styles from './style.less';
 
-import { StickyContainer, Sticky } from 'react-sticky';
+import defaultSettings from './../../../../config/defaultSettings';
 
+const { langs, langDefault } = defaultSettings;
 const { Option } = Select;
 const { RangePicker } = DatePicker;
 const { TabPane } = Tabs;
 const fieldLabels = {
   name: 'Nombre',
+  type: 'Tipo de producto',
+  lang: 'Idioma',
   url: '仓库域名',
   owner: '仓库管理员',
   approver: '审批人',
   dateRange: '生效日期',
-  type: '仓库类型',
   name2: '任务名',
   url2: '任务描述',
   owner2: '执行人',
@@ -81,18 +83,26 @@ const renderTabBar = (props, DefaultTabBar) => (
   </Sticky>
 );
 
-
 @connect(({ loading }: { loading: { effects: { [key: string]: boolean } } }) => ({
   submitting: loading.effects['formAdvancedForm/submitAdvancedForm'],
 }))
 class ProductForm extends Component<FormProps> {
   state = {
     width: '100%',
+    lang: 'es',
+    info: [],
   };
 
   componentDidMount() {
     window.addEventListener('resize', this.resizeFooterToolbar, { passive: true });
     this.resizeFooterToolbar();
+
+    this.setState(state => ({
+      ...state,
+      info: langs.map((item: any) => {
+        return { lang: item.id };
+      }),
+    }));
   }
 
   componentWillUnmount() {
@@ -114,23 +124,33 @@ class ProductForm extends Component<FormProps> {
         labelNode.scrollIntoView(true);
       }
     };
-    const errorList = Object.keys(errors).map(key => {
-      if (!errors[key]) {
-        return null;
-      }
-      const errorMessage = errors[key] || [];
-      return (
-        <li key={key} className={styles.errorListItem} onClick={() => scrollToField(key)}>
-          <Icon type="cross-circle-o" className={styles.errorIcon} />
-          <div className={styles.errorMessage}>{errorMessage[0]}</div>
-          <div className={styles.errorField}>{fieldLabels[key]}</div>
-        </li>
-      );
-    });
+    const errorList = Object.keys(errors)
+      .map(key => {
+        if (!errors[key]) {
+          return null;
+        }
+        const errorMessage = errors[key].title || errors[key][0] || null;
+        // console.log('errorMessage',errorMessage,'errors',errors[key],'key', key)
+        if (!errorMessage) {
+          return null;
+        }
+        return (
+          <li key={key} className={styles.errorListItem} onClick={() => scrollToField(key)}>
+            <Icon type="cross-circle-o" className={styles.errorIcon} />
+            <div className={styles.errorMessage}>{errorMessage}</div>
+            <div className={styles.errorField}>{fieldLabels[key]}</div>
+          </li>
+        );
+      })
+      .filter(item => item !== null);
+    console.log('errorList', errorList);
+    if (!errorList.length) {
+      return null;
+    }
     return (
       <span className={styles.errorIcon}>
         <Popover
-          title="表单校验信息"
+          title="Errores"
           content={errorList}
           overlayClassName={styles.errorPopover}
           trigger="click"
@@ -167,6 +187,7 @@ class ProductForm extends Component<FormProps> {
       dispatch,
     } = this.props;
     validateFieldsAndScroll((error, values) => {
+      console.log('submit values', values, error);
       if (!error) {
         // submit the values
         dispatch({
@@ -177,18 +198,81 @@ class ProductForm extends Component<FormProps> {
     });
   };
 
+  renderTitle = () => {
+    const {
+      form: { getFieldDecorator, getFieldValue },
+    } = this.props;
+    const productTypes = [
+      { id: 'single', label: 'Simple' },
+      { id: 'package', label: 'Paquete' },
+      { id: 'virtual', label: 'Virtual' },
+    ];
+    const lang = getFieldValue('lang');
+
+    return (
+      <Row gutter={8}>
+        <Col xs={24} md={12}>
+          {langs.map(itemLang => (
+            <Form.Item
+              label={fieldLabels.name}
+              style={itemLang.id === lang ? {} : { display: 'none' }}
+            >
+              {getFieldDecorator(`${itemLang.id}.title`, {
+                initialValue: null,
+                rules: [
+                  {
+                    required: true,
+                    message: `Nombre es obligatorio ${itemLang.label}`,
+                  },
+                ],
+              })(<Input type={itemLang.id === lang ? 'text' : 'hidden'} />)}
+            </Form.Item>
+          ))}
+        </Col>
+        <Col xs={12} md={3}>
+          <Form.Item label={fieldLabels.lang}>
+            {getFieldDecorator('lang', {
+              initialValue: langDefault,
+            })(
+              <Select>
+                {langs.map((lang: any) => (
+                  <Option value={lang.id}>{lang.label}</Option>
+                ))}
+              </Select>,
+            )}
+          </Form.Item>
+        </Col>
+
+        <Col xs={12} md={3}>
+          <Form.Item label={fieldLabels.type}>
+            {getFieldDecorator('type', {
+              initialValue: 'single',
+            })(
+              <Select>
+                {productTypes.map((pType: any) => (
+                  <Option value={pType.id}>{pType.label}</Option>
+                ))}
+              </Select>,
+            )}
+          </Form.Item>
+        </Col>
+      </Row>
+    );
+  };
+
   render() {
     const {
-      form: { getFieldDecorator },
+      form: { getFieldDecorator, getFieldValue },
       submitting,
     } = this.props;
-    const { width } = this.state;
+    const { width, info } = this.state;
+    const lang = getFieldValue('lang');
+    console.log('lang', lang, info);
     return (
       <>
-        <PageHeaderWrapper content="Editor de producto">
-          <StickyContainer>   
+        <PageHeaderWrapper content={this.renderTitle()}>
+          <StickyContainer>
             <Tabs defaultActiveKey="info" renderTabBar={renderTabBar}>
-
               <TabPane
                 tab={
                   <span>
@@ -198,9 +282,9 @@ class ProductForm extends Component<FormProps> {
                 }
                 key="info"
               >
-                {getFieldDecorator('info', {
-                initialValue: tableData,
-                })(<ProductInfo />)}
+                {getFieldDecorator(`${lang}.info`, {
+                  initialValue: tableData,
+                })(<ProductInfo submitting={submitting} />)}
               </TabPane>
 
               <TabPane
@@ -212,9 +296,9 @@ class ProductForm extends Component<FormProps> {
                 }
                 key="stock"
               >
-              {getFieldDecorator('stock', {
-                initialValue: tableData,
-                })(<ProductStock />)}
+                {getFieldDecorator('stock', {
+                  initialValue: tableData,
+                })(<ProductStock submitting={submitting} />)}
               </TabPane>
 
               <TabPane
@@ -227,10 +311,10 @@ class ProductForm extends Component<FormProps> {
                 key="shipping"
               >
                 {getFieldDecorator('stock', {
-                initialValue: tableData,
-                })(<ProductShipping />)}
+                  initialValue: tableData,
+                })(<ProductShipping submitting={submitting} />)}
               </TabPane>
-              
+
               <TabPane
                 tab={
                   <span>
@@ -241,8 +325,8 @@ class ProductForm extends Component<FormProps> {
                 key="pricing"
               >
                 {getFieldDecorator('pricing', {
-                initialValue: tableData,
-                })(<ProductPricing />)}
+                  initialValue: tableData,
+                })(<ProductPricing submitting={submitting} />)}
               </TabPane>
 
               <TabPane
@@ -255,10 +339,9 @@ class ProductForm extends Component<FormProps> {
                 key="seo"
               >
                 {getFieldDecorator('seo', {
-                initialValue: tableData,
+                  initialValue: tableData,
                 })(<ProductSeo />)}
               </TabPane>
-              
             </Tabs>
           </StickyContainer>
 
@@ -267,12 +350,11 @@ class ProductForm extends Component<FormProps> {
               initialValue: tableData,
             })(<TableForm />)}
           </Card> */}
-          
         </PageHeaderWrapper>
         <FooterToolbar style={{ width }}>
           {this.getErrorInfo()}
           <Button type="primary" onClick={this.validate} loading={submitting}>
-            提交
+            Guardar
           </Button>
         </FooterToolbar>
       </>
