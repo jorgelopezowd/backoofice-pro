34fc5587d477836809e6422cc4941755245ad694
jore lopez <jore.lopez@luckywoman.com.co>
2019-10-15 16:33:47 -0500

    animacion cambio de pagina

--- src/components/Authorized/AuthorizedRoute.jsx
+++ src/components/Authorized/AuthorizedRoute.jsx
@@ -1,5 +1,6 @@
 import { Redirect, Route } from 'umi';
 import React from 'react';
+import QueueAnim from 'rc-queue-anim';
 import Authorized from './Authorized';
 
 const AuthorizedRoute = ({ component: Component, render, authority, redirectPath, ...rest }) => (
@@ -18,7 +19,9 @@ const AuthorizedRoute = ({ component: Component, render, authority, redirectPath
       />
     }
   >
-    <Route {...rest} render={props => (Component ? <Component {...props} /> : render(props))} />
+    <QueueAnim type={['right', 'left']}>
+      <Route {...rest} render={props => (Component ? <Component {...props} /> : render(props))} />
+    </QueueAnim>
   </Authorized>
 );
 
