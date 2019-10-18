a3fe2e997248df7e473baac5707594cb76d4be24
jore lopez <jore.lopez@openwebdelopers.net>
2019-10-18 11:13:19 -0500

    test current

--- src/services/user.js
+++ src/services/user.js
@@ -7,7 +7,20 @@ export async function query() {
   return request('/api/users');
 }
 export async function queryCurrent() {
-  return currentService.find();
+  console.log('current app.authentication.service', app.authentication);
+  const current = app.service('current');
+  //test code
+  current
+    .find()
+    .then(data => console.log('current >> data', data))
+    .catch(err => {
+      console.log('current err >>', err);
+    });
+  // if(!app.authentication.authenticated){
+  //   await app.reAuthenticate()
+  // }
+
+  return app.service('current').find();
 }
 export async function queryNotices() {
   return request('/api/notices');
